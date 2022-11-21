366589a2e67f9898bb01d05986a42469a81d69eb
robles153 <marcoskim153@gmail.com>
2022-11-21 15:43:28 -0300

    Implementando a camada Application

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,14 +1,12 @@
-﻿using CleanArchMvc.Domain.Interfacees;
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Interfacees;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.Infra.IoC
 {
@@ -21,6 +19,11 @@ namespace CleanArchMvc.Infra.IoC
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
 
             return services;
         }
---
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿/*using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productSerive;
+        public ProductsController(IProductService productService)
+        {
+            _productSerive = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productSerive.GetProducts();
+            return View(products);
+        }
+    }
+}
+*/
+
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productAppService)
+        {
+            _productService = productAppService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProducts();
+            return View(products);
+        }
+    }
+}
