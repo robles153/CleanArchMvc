56449d75f9f9ae6272e4549b6daa51c0f0bb295e
robles153 <marcoskim153@gmail.com>
2022-12-01 16:09:33 -0300

    Implementando GetAll de Categories

--- CleanArchMvc.Infra.IoC/DependecyInjectionAPI.cs
+++ CleanArchMvc.Infra.IoC/DependecyInjectionAPI.cs
@@ -1,4 +1,4 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿/*using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
 using CleanArchMvc.Domain.Account;
@@ -35,6 +35,57 @@ namespace CleanArchMvc.Infra.IoC
 
             services.AddScoped<IAuthenticate, AuthenticateService>();
          
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(myhandlers);
+
+
+            return services;
+        }
+    }
+}
+*/
+
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Domain.Interfacees;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+                   options.AccessDeniedPath = "/Account/Login");
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+           
 
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
