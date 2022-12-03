bd31a49f87404053ba4a804b3d4e6c1cc0a4757e
robles153 <marcoskim153@gmail.com>
2022-12-03 11:50:37 -0300

    Implementando controlador ProductsController

--- CleanArcgMvc.API/Controllers/CategoriesController.cs
+++ CleanArcgMvc.API/Controllers/CategoriesController.cs
@@ -1,38 +1,4 @@
-﻿/*using CleanArchMvc.Application.DTOs;
-using CleanArchMvc.Application.Interfaces;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CleanArcgMvc.API.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoriesController : ControllerBase
-    {
-        private readonly ICategoryService _categoryService;
-        public CategoriesController(ICategoryService categoryService)
-        {
-            _categoryService = categoryService;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
-        {
-            var categories = await _categoryService.GetCategories();
-            if (categories == null)
-            {
-                return NotFound("Categories not found");
-            }
-            return Ok(categories);
-        }
-    }
-}
-*/
-using CleanArchMvc.Application.DTOs;
+﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
---
+++ CleanArcgMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArcgMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productsService;
+
+        public ProductsController(IProductService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var products = await  _productsService.GetProducts();
+            if (products == null)
+            {
+                return NotFound("Products not found.");
+            }
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> GetId(int id)
+        {
+            var product = await _productsService.GetById(id);
+            if (product == null)
+            {
+                return NotFound("Product not found.");
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDTO)
+        {
+            if (productDTO == null)
+            {
+                return BadRequest("Invalid Data");
+            }
+            await _productsService.Add(productDTO);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = productDTO });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO productDTO)
+        {
+            if (id != productDTO.Id)
+                return BadRequest();
+
+            if (productDTO == null)
+                return BadRequest();
+
+            await _productsService.Update(productDTO);
+
+            return Ok(productDTO);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id)
+        {
+            var product = await _productsService.GetById(id);
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+            await _productsService.Remove(id);
+            return Ok(product);
+        }
+    }
+}
