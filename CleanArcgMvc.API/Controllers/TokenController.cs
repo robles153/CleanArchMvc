c66cdfff199f3d5f204460107c462cf75ce58f10
robles153 <marcoskim153@gmail.com>
2022-12-22 14:15:07 -0300

    Implementando endpont CreateUser

--- CleanArcgMvc.API/Controllers/TokenController.cs
+++ CleanArcgMvc.API/Controllers/TokenController.cs
@@ -28,6 +28,23 @@ namespace CleanArcgMvc.API.Controllers
             _configuration = configuration;
         }
 
+        [HttpPost("CreateUser")]
+        public async Task<ActionResult> CreateUser([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.RegisterUser(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                
+                return Ok($"User {userInfo.Email} was create successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
         [HttpPost("LoginUser")]
         public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
         {
